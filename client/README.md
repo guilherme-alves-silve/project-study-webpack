833a0678d3a7c7cb2217a64f0c3dd700c472d401
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-08-10 15:15:44 -0300

    Adiciona compilador babel, cross-env e configuração webpack

--- client/README.md
+++ client/README.md
@@ -1,3 +1,18 @@
+# Projeto Study Webpack
+
+## Como executa o cliente
 Não esqueçam de entrar dentro da pasta `client` e executar o comando `npm install` antes de utilizar o projeto.
 
 Para ativar a compilação em tempo real, dentro de `client`, execute o comando `npm run watch`.
+
+## Como executar o servidor
+
+`npm start`
+
+## Como realizar o build:
+
+* Desenvolvimento:
+    * `npm run build-dev`
+
+* Produção
+    * `npm run build-prod`
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,28 @@
+const path = require("path");
+const BabiliPlugin = require("babili-webpack-plugin");
+
+let plugins = [];
+
+if (process.env.NODE_ENV == "production") {
+    plugins.push(new BabiliPlugin());
+}
+
 module.exports = {
     entry: "./app-src/app.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins
 }
